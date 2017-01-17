6c9d593667cc21415e2158cc4ad1c641dc696bec
Peter Alfvin <palfvin@gmail.com>
2017-01-16 17:58:55 -0800

    Eliminate subjects in favor of Observable.create

--- FrequentFlyer/Authentication/AuthMethodDataDeserializer.swift
+++ FrequentFlyer/Authentication/AuthMethodDataDeserializer.swift
@@ -7,30 +7,30 @@ class AuthMethodDataDeserializer {
         do {
             authMethodsJSONObject = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
         } catch { }
-
-        let subject = ReplaySubject<AuthMethod>.createUnbounded()
-
-        guard let authMethodsJSON = authMethodsJSONObject as? Array<NSDictionary> else {
-            subject.onError(DeserializationError(details: "Could not interpret data as JSON dictionary", type: .invalidInputFormat))
-            return subject
-        }
-
-        for authMethodsDictionary in authMethodsJSON {
-            guard let typeString = authMethodsDictionary["type"] as? String else { continue }
-            guard let urlString = authMethodsDictionary["auth_url"] as? String else { continue }
-
-            var type = AuthType.basic
-            if typeString == "basic" {
-                type = .basic
-            } else if typeString == "oauth" {
-                type = .github
+        
+        return Observable.create { observer in
+            guard let authMethodsJSON = authMethodsJSONObject as? Array<NSDictionary> else {
+                observer.onError(DeserializationError(details: "Could not interpret data as JSON dictionary", type: .invalidInputFormat))
+                return Disposables.create()
             }
-
-            subject.onNext(AuthMethod(type: type, url: urlString))
+            
+            for authMethodsDictionary in authMethodsJSON {
+                guard let typeString = authMethodsDictionary["type"] as? String else { continue }
+                guard let urlString = authMethodsDictionary["auth_url"] as? String else { continue }
+                
+                var type = AuthType.basic
+                if typeString == "basic" {
+                    type = .basic
+                } else if typeString == "oauth" {
+                    type = .github
+                }
+                
+                observer.onNext(AuthMethod(type: type, url: urlString))
+            }
+            
+            observer.onCompleted()
+            
+            return Disposables.create()
         }
-
-        subject.onCompleted()
-
-        return subject
     }
 }
--- FrequentFlyer/Authentication/AuthMethodsService.swift
+++ FrequentFlyer/Authentication/AuthMethodsService.swift
@@ -6,24 +6,24 @@ class AuthMethodsService {
     var authMethodsDataDeserializer = AuthMethodDataDeserializer()
 
     func getMethods(forTeamName teamName: String, concourseURL: String) -> Observable<AuthMethod> {
-        let dataSubject = PublishSubject<Data>()
-        let urlString = "\(concourseURL)/api/v1/teams/\(teamName)/auth/methods"
-        let url = URL(string: urlString)
-        let request = NSMutableURLRequest(url: url!)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "GET"
-        _ = self.httpClient.doRequest(request as URLRequest) { response, error in
-            if let error = error {
-                dataSubject.onError(error)
-                return
+        return Observable.create { observer in
+            let urlString = "\(concourseURL)/api/v1/teams/\(teamName)/auth/methods"
+            let url = URL(string: urlString)
+            let request = NSMutableURLRequest(url: url!)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "GET"
+            _ = self.httpClient.doRequest(request as URLRequest) { response, error in
+                if let error = error {
+                    observer.onError(error)
+                    return
+                }
+                
+                observer.onNext(response!.body!)
             }
-
-            dataSubject.onNext(response!.body!)
+            return Disposables.create()
         }
-
-        return dataSubject.flatMap {
-            data in
+        .flatMap { data in
             self.authMethodsDataDeserializer.deserialize(data)
-            }
+        }
     }
 }
