b5f0fbfd594a0ca5f77f9d18344ae9f0f7f56c3f
Jared Friese <jared.friese@gmail.com>
2017-03-26 20:46:02 -0700

    Remove obsolete `HTTPClient` interface
    [#138242913]

--- FrequentFlyer/HTTP/HTTPClient.swift
+++ FrequentFlyer/HTTP/HTTPClient.swift
@@ -9,31 +9,6 @@ class HTTPClient {
         session = URLSession(configuration: sessionConfig)
     }
 
-    func doRequest(_ request: URLRequest, completion: ((HTTPResponse?, FFError?) -> ())?) {
-        let dataTask = session.dataTask(with: request, completionHandler: { data, response, error in
-            guard let completion = completion else { return }
-            guard let response = response else {
-                guard let error = error else {
-                    completion(nil, BasicError(details: "Unexpected error - received no response and no error"))
-                    return
-                }
-
-                completion(nil, BasicError(details: error.localizedDescription))
-                return
-            }
-
-            guard let httpURLResponse = response as? HTTPURLResponse else {
-                completion(nil, BasicError(details: "HTTPClient only supports HTTP and HTTPS"))
-                return
-            }
-
-            let httpResponse = HTTPResponseImpl(body: data, statusCode: httpURLResponse.statusCode)
-            completion(httpResponse, nil)
-        })
-
-        dataTask.resume()
-    }
-
     func perform(request: URLRequest) -> Observable<HTTPResponse> {
         let $ = PublishSubject<HTTPResponse>()
 
