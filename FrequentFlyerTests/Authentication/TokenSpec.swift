6c464b68f609d78ac4c21d1a900b9ecadc5a7bac
Jared Friese <jared.friese@gmail.com>
2017-03-30 20:36:56 -0700

    Remove model equality operator tests
    - They do not add much value
    
    [#142697207]

--- FrequentFlyerTests/Authentication/TokenSpec.swift
+++ FrequentFlyerTests/Authentication/TokenSpec.swift
@@ -5,26 +5,6 @@ import Nimble
 
 class TokenSpec: QuickSpec {
     override func spec() {
-        describe("Equality operator") {
-            context("When all properties of the Token objects are equal") {
-                let tokenOne = Token(value: "turtle token value")
-                let tokenTwo = Token(value: "turtle token value")
-
-                it("returns true") {
-                    expect(tokenOne).to(equal(tokenTwo))
-                }
-            }
-
-            context("When the 'value' values of the Token objects differ") {
-                let tokenOne = Token(value: "turtle token value")
-                let tokenTwo = Token(value: "some other token value")
-
-                it("returns false") {
-                    expect(tokenOne).toNot(equal(tokenTwo))
-                }
-            }
-        }
-
         describe("Token.authValue") {
             context("When the token's value already includes 'Bearer ' as a prefix") {
                 it("returns the existing token value") {
