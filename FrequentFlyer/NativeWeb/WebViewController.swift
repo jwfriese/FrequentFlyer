18f9a774683bfc0dfb59c3df8651f2ad9e176e5c
Jared Friese <jared.friese@gmail.com>
2017-04-09 15:48:46 -0700

    Add navigation chrome to GitHub web view
    [#143327883]

--- FrequentFlyer/NativeWeb/WebViewController.swift
+++ FrequentFlyer/NativeWeb/WebViewController.swift
@@ -2,27 +2,38 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController {
+    @IBOutlet weak var contentView: UIView?
     var webPageURL: URL?
 
-    var webView: WKWebView? {
+    private weak var cachedWebView: WKWebView?
+    weak var webView: WKWebView? {
         get {
-            return view as? WKWebView
+            if cachedWebView != nil { return cachedWebView }
+
+            let webView = contentView?.subviews.filter { subview in
+                return subview.isKind(of: WKWebView.self)
+                }.first as? WKWebView
+
+            cachedWebView = webView
+            return cachedWebView
         }
     }
-
     class var storyboardIdentifier: String { get { return "Web" } }
-
-    override func loadView() {
-        view = WKWebView()
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let webPageURL = webPageURL else {
-            return
-        }
+        guard let webPageURL = webPageURL else { return }
+        guard let contentView = contentView else { return }
+
+        let webView = WKWebView()
+        contentView.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: webView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: webView.bottomAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: webView.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: webView.trailingAnchor).isActive = true
 
         let request = URLRequest(url: webPageURL)
-        let _ = webView?.load(request)
+        let _ = webView.load(request)
     }
 }
