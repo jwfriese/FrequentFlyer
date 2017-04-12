518c2a7d7d861e835c7b45acc50fea7020edbbb0
Jared Friese <jared.friese@gmail.com>
2017-04-11 22:28:51 -0700

    Add Crashlytics
    [#143554235]

--- FrequentFlyer/AppDelegate.swift
+++ FrequentFlyer/AppDelegate.swift
@@ -1,10 +1,14 @@
 import UIKit
+import Fabric
+import Crashlytics
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
+
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         window = UIWindow(frame: UIScreen.main.bounds)
         let navigationController = storyboard.instantiateInitialViewController() as? UINavigationController
