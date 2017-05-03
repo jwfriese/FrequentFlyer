70f6690afd2d987c42540472a02f5710fba8e40b
Ryan McLeod <rmcleod@pivotal.io>
2017-05-03 12:16:19 -0400

    Put network activity indicator back in.

--- FrequentFlyer/HTTP/HTTPClient.swift
+++ FrequentFlyer/HTTP/HTTPClient.swift
@@ -13,8 +13,14 @@ class HTTPClient {
 
     func perform(request: URLRequest) -> Observable<HTTPResponse> {
         return session.rx.response(request: request)
+            .do(onSubscribed: {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            })
             .map { response, data in
                 return HTTPResponseImpl(body: data, statusCode: response.statusCode)
             }
+            .do(onCompleted: {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            })
     }
 }
